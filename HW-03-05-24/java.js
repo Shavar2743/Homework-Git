8da3888629217d2d110e264fd887e078146d72c0
Shavar2743 <markus.shavar@web.de>
2024-05-03 18:30:32 +0200

    HW-03-05-24

---
+++ HW-03-05-24/java.js
@@ -0,0 +1,29 @@
+button1.onclick = function () {
+    if (header.style.color == "red" ) {
+        header.style.color = "";
+    } else {
+        header.style.color ="red";
+    }
+}
+
+button2.onclick = function () {
+    if (header.style.backgroundColor == "green" ) {
+        header.style.backgroundColor = "";
+    } else {
+        header.style.backgroundColor = "green"
+    }
+}
+
+button3.onclick = function () {
+    if (bild.style.display == "none") {
+        bild.style.display = "block"
+    } else {
+        bild.style.display = "none";
+    }
+}
+button4.onclick = function () {
+    bild.style.transform = "scale(0.9)";
+}
+button5.onclick = function () {
+    bild.style.transform = "scale(1.1)";
+}

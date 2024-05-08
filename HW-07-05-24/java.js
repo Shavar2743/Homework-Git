2b7fe22c729bf6ee23a50d5bdbef49eaef662493
Shavar2743 <markus.shavar@web.de>
2024-05-08 12:06:10 +0200

    update

--- HW-07-05-24/java.js
+++ HW-07-05-24/java.js
@@ -1,10 +1,14 @@
 eingabefeld.addEventListener("input", function jein() {
     const jahr = this.value;
-    if ((jahr % 4 === 0 && jahr % 100 !== 0) || jahr % 400 === 0) {
-        ausgabe.textContent = "JA";
-        ausgabe.style.color = "green";
+    if (isNaN(parseInt(jahr))) {
+        ausgabe.textContent = "";
     } else {
-        ausgabe.textContent = "NEIN";
-        ausgabe.style.color = "red";
+        if ((jahr % 4 === 0 && jahr % 100 !== 0) || jahr % 400 === 0) {
+            ausgabe.textContent = "JA";
+            ausgabe.style.color = "green";
+        } else {
+            ausgabe.textContent = "NEIN";
+            ausgabe.style.color = "red";
+        }
     }
 });
